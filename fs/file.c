2e4abee52c24ea9efc36cd0d2f29e1bbd397196a
Linus Torvalds <torvalds@linux-foundation.org>
2022-03-29 23:29:18 -0700

    UPSTREAM: fs: fix fd table size alignment properly
    [ Upstream commit d888c83fcec75194a8a48ccd283953bdba7b2550 ]
    
    Jason Donenfeld reports that my commit 1c24a186398f ("fs: fd tables have
    to be multiples of BITS_PER_LONG") doesn't work, and the reason is an
    embarrassing brown-paper-bag bug.
    
    Yes, we want to align the number of fds to BITS_PER_LONG, and yes, the
    reason they might not be aligned is because the incoming 'max_fd'
    argument might not be aligned.
    
    But aligining the argument - while simple - will cause a "infinitely
    big" maxfd (eg NR_OPEN_MAX) to just overflow to zero.  Which most
    definitely isn't what we want either.
    
    The obvious fix was always just to do the alignment last, but I had
    moved it earlier just to make the patch smaller and the code look
    simpler.  Duh.  It certainly made _me_ look simple.
    
    Fixes: 1c24a186398f ("fs: fd tables have to be multiples of BITS_PER_LONG")
    Reported-and-tested-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Cc: Fedor Pchelkin <aissur0002@gmail.com>
    Cc: Alexey Khoroshilov <khoroshilov@ispras.ru>
    Cc: Christian Brauner <brauner@kernel.org>
    Change-Id: I6d3a0c28896e8cf8cab30f60679aab785aeee193
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- fs/file.c
+++ fs/file.c
@@ -273,7 +273,7 @@ static unsigned int sane_fdtable_size(struct fdtable *fdt, unsigned int max_fds)
 	count = count_open_files(fdt);
 	if (max_fds < NR_OPEN_DEFAULT)
 		max_fds = NR_OPEN_DEFAULT;
-	return min(count, max_fds);
+	return ALIGN(min(count, max_fds), BITS_PER_LONG);
 }
 
 /*
